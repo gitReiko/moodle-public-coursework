e2fc75db0d8361514725f12c14e42f8763b7090a
gitReiko <khornau@gmail.com>
2019-02-11 15:38:23 +0300

    - now deleted themes that are used are transferred to ownthemes field.

--- classes/configuration/themes_management/themes_management_database_event_handler.php
+++ classes/configuration/themes_management/themes_management_database_event_handler.php
@@ -50,9 +50,40 @@ class ThemesManagementDatabaseEventHandler
 
     private function delete_theme() : void
     {
+        $themeID = optional_param(THEME.ID, 0 , PARAM_INT);
+
+        if($this->is_theme_already_used($themeID))
+        {
+            $this->replace_deleted_theme_with_own_theme_for_all_students($themeID);
+        }
+
         global $DB;
-        $id = optional_param(THEME.ID, 0 , PARAM_INT);
-        $DB->delete_records('coursework_themes', array('id'=>$id));
+        $DB->delete_records('coursework_themes', array('id'=>$themeID));
+    }
+
+    private function is_theme_already_used(int $themeID) : bool 
+    {
+        global $DB;
+
+        if($DB->count_records('coursework_students', array('theme'=>$themeID))) return true;
+        else return false;
+    }
+
+    private function replace_deleted_theme_with_own_theme_for_all_students(int $themeID) : void
+    {
+        global $DB;
+        $students = $DB->get_records('coursework_students', array('theme'=>$themeID));
+        $theme = $DB->get_record('coursework_themes', array('id'=>$themeID));
+
+        foreach($students as $student)
+        {
+            $temp = new stdClass;
+            $temp->id = $student->id;
+            $temp->theme = null;
+            $temp->owntheme = $theme->name;
+
+            $DB->update_record('coursework_students', $temp);
+        }
     }
 
 
