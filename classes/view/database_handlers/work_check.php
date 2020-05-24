1a9ea22328db525568e55a050fd2222dfc9c3df8
reiko <khornau@gmail.com>
2020-05-24 13:01:52 +0300

    work check improv

--- classes/view/database_handlers/work_check.php
+++ classes/view/database_handlers/work_check.php
@@ -1,6 +1,7 @@
 <?php
 
 use coursework_lib as lib;
+use view_lib as view;
 
 class WorkCheckDatabaseHandler 
 {
@@ -62,19 +63,106 @@ class WorkCheckDatabaseHandler
     {
         if($this->work->status == READY)
         {
-            $grade = new stdClass;
-            $grade->userid   = $this->work->student;
-            $grade->rawgrade = $this->work->grade;
-            $coursework = lib\get_coursework($this->cm->instance);
-            coursework_grade_item_update($coursework, $grade);
-        }
+            $this->save_grade_in_gradebook();
 
+            if(view\is_coursework_use_task($this->cm))
+            {
+                $this->update_user_task_sections_to_ready();
+            }
+        }
 
         global $DB;
         return $DB->update_record('coursework_students', $this->work);
     }
 
-    
+    private function save_grade_in_gradebook() : void 
+    {
+        $grade = new stdClass;
+        $grade->userid   = $this->work->student;
+        $grade->rawgrade = $this->work->grade;
+        $coursework = lib\get_coursework($this->cm->instance);
+        coursework_grade_item_update($coursework, $grade);
+    }
 
+    private function update_user_task_sections_to_ready() : void 
+    {
+        $sections = lib\get_sections_to_check($this->cm, $this->work->student);
+
+        foreach($sections as $section)
+        {
+            $sectionRow = $this->get_section($section->id);
+            if($this->is_student_task_section_exist($section->id))
+            {
+                if($this->is_section_status_not_ready($section->id))
+                {
+                    $sectionRow->id = $this->get_section_id($section->id);
+                    $this->update_section_in_database($sectionRow);
+                }
+            }
+            else
+            {
+                $this->insert_section_to_database($sectionRow);
+            }
+        }
+    }
+
+    private function is_student_task_section_exist(int $sectionId) : bool 
+    {
+        global $DB;
+        $conditions = array('coursework' => $this->cm->instance,
+                            'student' => $this->work->student,
+                            'section' => $sectionId);
+        return $DB->record_exists('coursework_sections_status', $conditions);
+    }
+
+    private function is_section_status_not_ready(int $sectionId) : bool 
+    {
+        global $DB;
+        $sql = 'SELECT * 
+                FROM {coursework_sections_status}
+                WHERE coursework = ?
+                AND student = ? 
+                AND section = ? 
+                AND status != ?';  
+        $params = array($this->cm->instance, $this->work->student, $sectionId, READY);
+        return $DB->record_exists_sql($sql, $params);
+    }
+
+    private function get_section(int $sectionId) : stdClass 
+    {
+        $section = new stdClass;
+        $section->coursework = $this->cm->instance;
+        $section->student = $this->work->student;
+        $section->section = $sectionId;
+        $section->status = READY;
+        $section->timemodified = time();
+        return $section;
+    }
+
+    private function insert_section_to_database(stdClass $section) : void 
+    {
+        global $DB;
+        $DB->insert_record('coursework_sections_status', $section);
+    }
+
+    private function get_section_id(int $sectionId) : int 
+    {
+        global $DB;
+        $conditions = array('coursework' => $this->cm->instance,
+                            'student' => $this->work->student,
+                            'section' => $sectionId);
+        return $DB->get_field('coursework_sections_status', 'id', $conditions);
+    }
     
+    private function update_section_in_database(stdClass $section) : void 
+    {
+        global $DB;
+        $DB->update_record('coursework_sections_status', $section);
+    }
+
+
+
+
+
+
 }
