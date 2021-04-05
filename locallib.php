e8760495c702adab216e7f373755b6aa4db732c1
Denis Makouski (Reiko) <khornau@gmail.com>
2021-04-05 21:23:41 +0300

    getters impr

--- locallib.php
+++ locallib.php
@@ -25,9 +25,40 @@ class LocalLib
         return groups_get_activity_allowed_groups($cm);
     }
 
+    public static function get_students_from_grade_item()
+    {
+        // function get_enrolled_users(context $context, $withcapability = '', $groupid = 0, $userfields = 'u.*', $orderby = '', $limitfrom = 0, $limitnum = 0)
+    }
 
+    public static function get_teachers(int $courseworkId)
+    {
+        $teachers = self::get_configurated_teachers($courseworkId);
+    
+        return $teachers;
+    }
 
-
+    /**
+     * Returns teachers from coursework_teachers database table.
+     */
+    private static function get_configurated_teachers(int $courseworkId)
+    {
+        global $DB;
+        $sql = 'SELECT ct.id as recordid, ct.teacher as teacherid, u.firstname, u.lastname, 
+                    ct.course as courseid, c.fullname as coursename, ct.quota
+                FROM {coursework_teachers} as ct, {user} as u, {course} as c
+                WHERE ct.teacher = u.id AND ct.course = c.id AND coursework = ?';
+        $conditions = array($courseworkId);
+    
+        $teachers = $DB->get_records_sql($sql, $conditions);
+        $teachers = cw_add_fullnames_to_users_array($teachers);
+    
+        usort($teachers, function($a, $b)
+        {
+            return strcmp($a->fullname, $b->fullname);
+        });
+    
+        return $teachers;
+    }
 
 
 
