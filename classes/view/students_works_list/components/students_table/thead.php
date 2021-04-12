642069e43090921c5feca200f4233c3ada540177
Denis Makouski (Reiko) <khornau@gmail.com>
2021-04-12 08:55:10 +0300

    thead refact

--- classes/view/students_works_list/components/students_table/thead.php
+++ classes/view/students_works_list/components/students_table/thead.php
@@ -12,34 +12,63 @@ class Thead
         $head = \html_writer::start_tag('thead');
         $head.= \html_writer::start_tag('tr');
 
-        $attr = array('title' => get_string('notifications', 'coursework'));
-        $text = '<i class="fa fa-exclamation-triangle"></i>';
-        $head.= \html_writer::tag('td', $text, $attr);
-
-        $attr = array('title' => get_string('more_details', 'coursework'));
-        $text = '<i class="fa fa-arrow-down"></i>';
-        $head.= \html_writer::tag('td', $text, $attr);
-
-        $attr = array('title' => get_string('go_to_student_work', 'coursework'));
-        $text = get_string('work', 'coursework');
-        $head.= \html_writer::tag('td', $text, $attr);
-
-        $text = get_string('student', 'coursework');
-        $head.= \html_writer::tag('td', $text);
-
-        $text = get_string('state', 'coursework');
-        $head.= \html_writer::tag('td', $text);
-
-        $text = get_string('theme', 'coursework');
-        $head.= \html_writer::tag('td', $text);
-
-        $text = get_string('grade_short', 'coursework');
-        $head.= \html_writer::tag('td', $text);
+        $head.= $this->get_notifications_cell();
+        $body.= $this->get_more_details_cell();
+        $body.= $this->get_go_to_work_cell();
+        $body.= $this->get_student_cell();
+        $body.= $this->get_state_cell();
+        $body.= $this->get_theme_cell();
+        $body.= $this->get_grade_cell();
 
         $head.= \html_writer::end_tag('tr');
         $head.= \html_writer::end_tag('thead');
 
         return $head;
+    }
+
+    private function get_notifications_cell() : string 
+    {
+        $attr = array('title' => get_string('notifications', 'coursework'));
+        $text = '<i class="fa fa-exclamation-triangle"></i>';
+        return \html_writer::tag('td', $text, $attr); 
+    }
+
+    private function get_more_details_cell() : string 
+    {
+        $attr = array('title' => get_string('more_details', 'coursework'));
+        $text = '<i class="fa fa-arrow-down"></i>';
+        return \html_writer::tag('td', $text, $attr);
+    }
+
+    private function get_go_to_work_cell() : string 
+    {
+        $attr = array('title' => get_string('go_to_student_work', 'coursework'));
+        $text = get_string('work', 'coursework');
+        return \html_writer::tag('td', $text, $attr);
+    }
+
+    private function get_student_cell() : string 
+    {
+        $text = get_string('student', 'coursework');
+        return \html_writer::tag('td', $text);
+    }
+
+    private function get_state_cell() : string 
+    {
+        $text = get_string('state', 'coursework');
+        return \html_writer::tag('td', $text);
+    }
+
+    private function get_theme_cell() : string 
+    {
+        $text = get_string('theme', 'coursework');
+        return \html_writer::tag('td', $text);
+    }
+
+    private function get_grade_cell() : string 
+    {
+        $text = get_string('grade_short', 'coursework');
+        return \html_writer::tag('td', $text);
     }
 
 
