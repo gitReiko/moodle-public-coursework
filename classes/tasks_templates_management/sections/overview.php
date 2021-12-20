e4d1aa6fcd202526d22ff358b3ec7dee9fba5467
Denis <denis.makouski@bseu.by>
2021-12-20 11:16:55 +0300

    Overview refact

--- classes/tasks_templates_management/sections/overview.php
+++ classes/tasks_templates_management/sections/overview.php
@@ -45,29 +45,33 @@ class Overview
 
     private function get_overview_header() : string 
     {
-        $header = '<h3>'.get_string('task_sections_list', 'coursework');
-        $header.= ' <b>'.$this->task->name.'</b></h3>';
-        return $header;
+        $text = get_string('task_sections_list', 'coursework').' ';
+        $text.= \html_writer::tag('b', $this->task->name);
+        return \html_writer::tag('h3', $text);
     }
 
     private function get_sections_table() : string 
     {
-        $table = '<table class="leaders_overview">';
+        $attr = array('class' => 'leaders_overview');
+        $table = \html_writer::start_tag('table', $attr);
         $table.= $this->get_sections_table_header();
         $table.= $this->get_sections_table_body();
-        $table.= '</table>';
+        $table.= \html_writer::end_tag('table');
+
         return $table;
     }
 
     private function get_sections_table_header() : string 
     {
-        $header = '<tr class="header">';
-        $header.= '<td>'.get_string('name', 'coursework').'</td>';
-        $header.= '<td>'.get_string('description', 'coursework'). '</td>';
-        $header.= '<td>'.get_string('completion_date', 'coursework'). '</td>';
-        $header.= '<td></td>';
-        $header.= '</tr>';
-        return $header;
+        $attr = array('class' => 'header');
+        $head = \html_writer::start_tag('tr', $attr);
+        $head.= \html_writer::tag('td', get_string('name', 'coursework'));
+        $head.= \html_writer::tag('td', get_string('description', 'coursework'));
+        $head.= \html_writer::tag('td', get_string('completion_date', 'coursework'));
+        $head.= \html_writer::tag('td', '');
+        $head.= \html_writer::end_tag('tr');
+
+        return $head;
     }
 
     private function get_sections_table_body() : string 
@@ -76,21 +80,23 @@ class Overview
 
         foreach($this->sections as $section)
         {
-            $body.= '<tr>';
-            $body.= '<td>'.$section->name.'</td>';
-            $body.= '<td style="max-width: 450px;">'.$section->description.'</td>';
+            $body.= \html_writer::start_tag('tr');
+            $body.= \html_writer::tag('td', $section->name);
+            $attr = array('style' => 'max-width: 450px;');
+            $body.= \html_writer::tag('td', $section->description, $attr);
 
             if(empty($section->completiondate))
             {
-                $body.= '<td></td>';
+                $body.= \html_writer::tag('td', '');
             }
-            else
+            else 
             {
-                $body.= '<td>'.date('d-m-Y', $section->completiondate).'</td>';
+                $text = date('d-m-Y', $section->completiondate);
+                $body.= \html_writer::tag('td', $text);
             }
-            
-            $body.= '<td>'.$this->get_edit_button($section).'</td>';
-            $body.= '</tr>';
+
+            $body.= \html_writer::tag('td', $this->get_edit_button($section));
+            $body.= \html_writer::end_tag('tr');
         }
 
         return $body;
@@ -98,44 +104,126 @@ class Overview
 
     private function get_edit_button(\stdClass $section) : string 
     {
-        $button = '<form method="post">';
-        $button.= '<input type="submit" value="'.get_string('edit', 'coursework').'">';
-        $button.= '<input type="hidden"'.Main::ID.'" value="'.$this->cm->id.'" >';
-        $button.= '<input type="hidden" name="'.Main::GUI_TYPE.'" value="'.Main::EDIT_SECTION.'">';
-        $button.= '<input type="hidden" name="'.Main::TASK_ID.'" value="'.$this->task->id.'">';
-        $button.= '<input type="hidden" name="'.Main::SECTION_ID.'" value="'.$section->id.'">';
-        $button.= '</form>';
-        return $button;
+        $attr = array('method' => 'post');
+        $btn = \html_writer::start_tag('form', $attr);
+
+        $attr = array(
+            'type' => 'submit',
+            'value' => get_string('edit', 'coursework')
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::ID,
+            'value' => $this->cm->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::GUI_TYPE,
+            'value' => Main::EDIT_SECTION
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::TASK_ID,
+            'value' => $this->task->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::SECTION_ID,
+            'value' => $section->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $btn.= \html_writer::end_tag('form');
+
+        return $btn;
     }
 
     private function get_buttons_panel() : string 
     {
-        $btns = '<table class="btns_panel"><tr>';
-        $btns.= '<td>'.$this->get_add_task_section_button().'</td>';
-        $btns.= '<td>'.$this->get_back_to_overview_button().'</td>';
-        $btns.= '</tr></table>';
+        $attr = array('class' => 'btns_panel');
+        $btns = \html_writer::start_tag('table', $attr);
+        $btns.= \html_writer::start_tag('tr');
+        $btns.= \html_writer::tag('td', $this->get_add_task_section_button());
+        $btns.= \html_writer::tag('td', $this->get_back_to_overview_button());
+        $btns.= \html_writer::end_tag('tr');
+        $btns.= \html_writer::end_tag('table');
+
         return $btns;
     }
 
     private function get_add_task_section_button() : string 
     {
-        $button = '<form method="post">';
-        $button.= '<input type="submit" value="'.get_string('add_task_section', 'coursework').'" autofocus>';
-        $button.= '<input type="hidden" name="'.Main::ID.'" value="'.$this->cm->id.'" >';
-        $button.= '<input type="hidden" name="'.Main::GUI_TYPE.'" value="'.Main::ADD_SECTION.'">';
-        $button.= '<input type="hidden" name="'.Main::TASK_ID.'" value="'.$this->task->id.'">';
-        $button.= '</form>';
-        return $button;
+        $attr = array('method' => 'post');
+        $btn = \html_writer::start_tag('form', $attr);
+
+        $attr = array(
+            'type' => 'submit',
+            'value' => get_string('add_task_section', 'coursework')
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::ID,
+            'value' => $this->cm->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::GUI_TYPE,
+            'value' => Main::ADD_SECTION
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::TASK_ID,
+            'value' => $this->task->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $btn.= \html_writer::end_tag('form');
+
+        return $btn;
     }
 
     private function get_back_to_overview_button() : string 
     {
-        $button = '<form method="post">';
-        $button.= '<input type="hidden" name="'.Main::ID.'" value="'.$this->cm->id.'" >';
-        $button.= '<input type="hidden" name="'.Main::GUI_TYPE.'" value="'.Main::OVERVIEW.'">';
-        $button.= '<input type="submit" value="'.get_string('back', 'coursework').'" >';
-        $button.= '</form>';
-        return $button;
+        $attr = array('method' => 'post');
+        $btn = \html_writer::start_tag('form', $attr);
+
+        $attr = array(
+            'type' => 'submit',
+            'value' => get_string('back', 'coursework')
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::ID,
+            'value' => $this->cm->id
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::GUI_TYPE,
+            'value' => Main::OVERVIEW
+        );
+        $btn.= \html_writer::empty_tag('input', $attr);
+
+        $btn.= \html_writer::end_tag('form');
+
+        return $btn;
     }
     
 
