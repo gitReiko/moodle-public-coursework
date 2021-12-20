854b707f5c4ccc8403dd788e8e3a6561e40f990b
Denis <denis.makouski@bseu.by>
2021-12-20 09:22:19 +0300

    Add refact

--- classes/tasks_templates_management/tasks/add.php
+++ classes/tasks_templates_management/tasks/add.php
@@ -14,7 +14,7 @@ class Add extends Action
 
     protected function get_action_header() : string
     {
-        return '<h3>'.get_string('add_task_header', 'coursework').'</h3>';
+        return \html_writer::tag('h3', get_string('add_task_header', 'coursework'));
     }
 
     protected function get_name_input_value() : string
@@ -29,12 +29,22 @@ class Add extends Action
 
     protected function get_action_button() : string
     {
-        return '<p><input type="submit" value="'.get_string('add_task_template', 'coursework').'" ></p>';
+        $attr = array(
+            'type' => 'submit',
+            'value' => get_string('add_task_template', 'coursework')
+        );
+        $input = \html_writer::empty_tag('input', $attr);
+        return \html_writer::tag('p', $input);
     }
 
     protected function get_unique_form_hidden_inputs() : string
     {
-        return '<input type="hidden" name="'.Main::DATABASE_EVENT.'" value="'.Main::ADD_TASK.'"/>';
+        $attr = array(
+            'type' => 'hidden',
+            'name' => Main::DATABASE_EVENT,
+            'value' => Main::ADD_TASK
+        );
+        return \html_writer::empty_tag('input', $attr);
     }
 
 }
