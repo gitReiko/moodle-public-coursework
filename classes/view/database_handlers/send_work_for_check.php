6cc9526c17037615a001fa8ca18b99587984fd8e
Denis <denis.makouski@bseu.by>
2022-04-19 10:19:29 +0300

    send work for check refact

--- classes/view/database_handlers/send_work_for_check.php
+++ classes/view/database_handlers/send_work_for_check.php
@@ -2,6 +2,7 @@
 
 namespace Coursework\View\DatabaseHandlers;
 
+use Coursework\View\DatabaseHandlers\Lib\AddNewStudentWorkStatus;
 use Coursework\View\DatabaseHandlers\Main as MainDB;
 use Coursework\Lib\Getters\StudentsGetter as sg;
 use Coursework\Lib\Getters\CommonGetter as cg;
@@ -14,7 +15,6 @@ class SendWorkForCheck
     private $cm;
 
     private $work;
-    private $status;
 
     function __construct(\stdClass $course, \stdClass $cm)
     {
@@ -22,28 +22,27 @@ class SendWorkForCheck
         $this->cm = $cm;
 
         $this->work = $this->get_student_work();
-        $this->status = $this->get_status();
     }
 
     public function handle()
     {
-        global $DB;
-        if($DB->insert_record('coursework_students_statuses', $this->status)) 
+        $addNewStatus = new AddNewStudentWorkStatus(
+            $this->work->coursework, 
+            $this->work->student, 
+            Enums::SENT_FOR_CHECK 
+        );
+        
+        if($addNewStatus->execute())
         {
             $this->send_notification();
             $this->log_event_student_sent_work_for_check();
-        }
-        else 
-        {
-            throw new \Exception('Student task state "sent_for_check" not added.');
         }
     }
 
     private function get_student_work() : \stdClass 
     {
-        $student = $this->get_student_id_from_request();
-        $student = sg::get_student_work($this->cm->instance, $student);
-        return $student;
+        $studentId = $this->get_student_id_from_request();
+        return sg::get_student_work($this->cm->instance, $studentId);
     }
 
     private function get_student_id_from_request() : int 
@@ -51,19 +50,6 @@ class SendWorkForCheck
         $student = optional_param(MainDB::STUDENT, null, PARAM_INT);
         if(empty($student)) throw new Exception('Missing student id.');
         return $student;
-    }
-
-    private function get_status() : \stdClass 
-    {
-        $state = new \stdClass;
-        $state->coursework = $this->work->coursework;
-        $state->student = $this->work->student;
-        $state->type = Enums::COURSEWORK;
-        $state->instance = $this->work->coursework;
-        $state->status = Enums::SENT_FOR_CHECK;
-        $state->changetime = time();
-
-        return $state;
     }
 
     private function send_notification() : void 
@@ -99,6 +85,5 @@ class SendWorkForCheck
         $event = \mod_coursework\event\student_sent_work_for_check::create($params);
         $event->trigger();
     }
-
 
 }
