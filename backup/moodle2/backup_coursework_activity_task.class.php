13f875d988510818cfdef79366907252c1972678
Denis <denis.makouski@bseu.by>
2022-05-17 10:31:58 +0300

    moodle 2.0 backup links encode

--- backup/moodle2/backup_coursework_activity_task.class.php
+++ backup/moodle2/backup_coursework_activity_task.class.php
@@ -29,7 +29,64 @@ class backup_coursework_activity_task extends backup_activity_task {
      * Code the transformations to perform in the activity in
      * order to get transportable (encoded) links
      */
-    static public function encode_content_links($content) {
+    static public function encode_content_links($content) 
+    {
+        global $CFG;
+
+        $base = preg_quote($CFG->wwwroot,"/");
+
+        // Default links
+        $search="/(".$base."\/mod\/coursework\/index.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@CHOICEINDEX*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/view.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@CHOICEVIEWBYID*$2@$', $content);
+
+        // Config pages
+        $search="/(".$base."\/mod\/coursework\/pages\/config\/list.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@CONFIGLISTID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/config\/appoint_leaders.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@APPOINTLEADERID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/config\/distribute_to_leaders.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@DISTRIBUTETOLEADERSID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/config\/set_default_task_template.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@SETDEFAULTTASKTEMPLATEID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/config\/set_suggested_themes.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@SETSUGGESTEDTHEMESID*$2@$', $content);
+
+        // Support pages
+        $search="/(".$base."\/mod\/coursework\/pages\/support\/list.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@SUPPORTLISTID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/support\/delete_student_coursework.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@DELETESTUDENTCOURSEWORKID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/support\/leader_replacement.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@LEADERREPLACEMENTID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/support\/return_to_theme_selection.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@RETURNTOTHEMESELECTIONID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/support\/return_work_for_rework.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@RETURNWORKFORREWORKID*$2@$', $content);
+
+        // Other pages
+        $search="/(".$base."\/mod\/coursework\/pages\/manage_old_files_area.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@MANAGEOLDFILESAREAID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/quota_overview.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@QUOTAOVERVIEWID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/tasks_templates_management.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@TASKSTEMPLATESMANAGEMENTID*$2@$', $content);
+
+        $search="/(".$base."\/mod\/coursework\/pages\/themes_collections_management.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@THEMESCOLLECTIONSMANAGEMENTID*$2@$', $content);
+
         return $content;
     }
 }
